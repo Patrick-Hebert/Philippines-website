69380e09037d34722a58251cc14313e96cb3f8aa
Patrick Hebert <122138601+Patrick-Hebert@users.noreply.github.com>
2026-05-06 21:01:32 -0700

    Improving homepage layout and image viewer

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["73.181.132.170/32"]
+    cidr_blocks = ["0.0.0.0/0"] # temporary, replace with 73.181.132.170/32
   }
 
   ingress {
