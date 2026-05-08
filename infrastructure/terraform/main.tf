7f975a8a75ef7bdeb1e4a4cb007ca5a7b15c4e0a
Patrick Hebert <122138601+Patrick-Hebert@users.noreply.github.com>
2026-05-08 13:10:46 -0700

    update aws s3 bucket policy terraform block

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -137,6 +137,10 @@ resource "aws_s3_bucket_public_access_block" "website" {
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
+  depends_on = [
+    aws_s3_bucket_public_access_block.website
+  ]
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
