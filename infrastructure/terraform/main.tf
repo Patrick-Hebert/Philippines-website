89c30d2b460b0f895021d4db4d06af435abd8947
Patrick Hebert <122138601+Patrick-Hebert@users.noreply.github.com>
2026-05-07 20:53:42 -0700

    add s3 bucket, public access block, policy and website config

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -109,3 +109,38 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "philippines-travel-site-bucket" 
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+    }]
+  })
+}
